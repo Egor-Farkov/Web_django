285e73015f52d6b69fdd7270a92fc782c9aebdc3
Egor Farkov <fef84@icloud.com>
2025-07-03 00:27:07 +0800

    Создано приложение Каталог

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import view_home, view_contact, detail_product
+from catalog.views import ProductContactView, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", view_home, name='home'),
-    path("contacts/", view_contact, name='contacts'),
-    path("detail_product/<int:pk>", detail_product, name='detail_product'),
+    path("", ProductListView.as_view(), name='home'),
+    path("contacts/", ProductContactView.as_view(), name='contacts'),
+    path("detail_product/<int:pk>", ProductDetailView.as_view(), name='detail_product'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls")),
+    path("", include("blogs.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
